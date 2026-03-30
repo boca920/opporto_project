72bbf38fa9c3d709c6b5d377c057887cfe2a0534
LAZER <alaamahmoudcode200@gmail.com>
2026-03-30 03:05:29 +0200

    Edit company_jop

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
         home: Home());
 
     //     return MaterialApp(
